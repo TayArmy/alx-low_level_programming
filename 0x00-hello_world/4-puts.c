b386e1ef61fc692e5f4215a0d7d3b03dab18a4f5
TayArmy <alabenitemitayo@gmail.com>
2022-09-08 09:20:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-1  /**
-2  * main - Entry point
-3  *
-4  * Return: Always 0 (Success)
-5  */
-7  int main(void)
-8  {
-9       puts ("Programming is like building a multilingual puzzle\n");
-10	return (0);
-11 }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
