fff5b304a24e6b62302f2055e9cd1a30549bc858
TayArmy <alabenitemitayo@gmail.com>
2022-09-08 08:59:44 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+1  /**
+2  * main - Entry point
+3  *
+4  * Return: Always 0 (Success)
+5  */
+7  int main(void)
+8  {
+9       puts ("Programming is like building a multilingual puzzle\n");
+10	return (0);
+11 }
