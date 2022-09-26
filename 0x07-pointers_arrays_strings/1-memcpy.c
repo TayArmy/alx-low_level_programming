f0bbfc31254385cb8d8c3e9600c1bb4e5a836375
TayArmy <alabenitemitayo@gmail.com>
2022-09-26 06:50:49 -0700

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - input
+ * @des: input pointer to string destination
+ * @sr: input pointer to source type string
+ * @n: number of bytes
+ * Return: pointer to destination string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+*(des + i) = *(sr + i);
+i++;
+}
+return (des);
+}
---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - a function that sets the value of a pointer to a char.
+ * @s: double pointer to a string
+ * @d: pointer to the string to set s to
+ * Return: Description of the returned value
+ */
+oid set_string(char **s, char *d)
+{
+	*s = d;
+}
