4890066dbfb914624fbc04fbb9ee1d1adae2a3d9
TayArmy <alabenitemitayo@gmail.com>
2022-10-06 13:24:46 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _puts - prints a string, followed by a new line,
  * @str: pointer to the string to print
  * Return: void
  */
-
 
 void _puts(char *str)
 {
