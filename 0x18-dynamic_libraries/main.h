7008fbc2f884fafbb0f87823c0560399bc7ad408
TayArmy <alabenitemitayo@gmail.com>
2022-12-12 20:03:31 -0800

    dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int mod(int, int);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
