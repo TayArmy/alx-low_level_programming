0cd9a0331a6222f283a7ac9a09e836fb5082f8e7
TayArmy <alabenitemitayo@gmail.com>
2023-05-18 19:19:30 -0700

    loopy

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
-/**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
- */
+
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 1, b = 2, next = 0;
+    int i = 0;
+    unsigned long int a = 1, b = 2, next = 0;
 
-	while (i < 97)
-	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
+    while (i < 97)
+    {
+        next = a + b;
+        a = b;
+        b = next;
+        printf("%lu", next);
 
-		if (i < 97)
-			printf(", ");
-		i++;
-	}
-	putchar('\n');
-	return (0);
+        if (i < 96)
+            printf(", ");
+        i++;
+    }
+    putchar('\n');
+    return 0;
 }
